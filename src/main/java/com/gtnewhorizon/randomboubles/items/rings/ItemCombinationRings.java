726ec928472557a32b8d9899b696d0dc40b443d4
GitHub GTNH Actions <>
2026-02-14 22:20:17 +0000

    spotlessApply

--- src/main/java/com/gtnewhorizon/randomboubles/items/rings/ItemCombinationRings.java
+++ src/main/java/com/gtnewhorizon/randomboubles/items/rings/ItemCombinationRings.java
@@ -16,7 +16,6 @@ import net.minecraft.util.IIcon;
 import net.minecraft.util.MathHelper;
 import net.minecraft.util.StatCollector;
 
-import com.dreammaster.item.NHItemList;
 import com.gtnewhorizon.randomboubles.RandomBoubles;
 
 import cpw.mods.fml.relauncher.Side;
